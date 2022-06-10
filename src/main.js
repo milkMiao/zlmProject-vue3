c081e352afa3cdef90fc54b0deba1293d291b0f5
ZLM <2479326918@qq.com>
2022-06-10 17:21:01 +0800

    删除文件

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Store from './store/index'
-import element3 from 'element3'
-
-createApp(App).use(Store).use(element3).mount('#app')
