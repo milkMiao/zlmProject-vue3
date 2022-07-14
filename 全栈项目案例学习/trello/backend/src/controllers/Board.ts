eb0fd8e9281ce1f915accc39a4bfb058231d943d
ZLM <2479326918@qq.com>
2022-07-14 17:05:16 +0800

    后端面板API实现与测试中prerequest的应用--Postman

--- 全栈项目案例学习/trello/backend/src/controllers/Board.ts
+++ 全栈项目案例学习/trello/backend/src/controllers/Board.ts
@@ -1,10 +1,9 @@
-import { Body, Controller,Get, Params, Post ,Put, Delete ,Header, Query, Ctx, Flow} from 'koa-ts-controllers';
+import { Body, Controller,Get, Params, Post ,Put, Delete ,Ctx, Flow} from 'koa-ts-controllers';
 import { Context } from 'koa';
-import { IsNumberString } from 'class-validator';
+import authorization  from '../middlewares/authorization' //权限
 import { PostAddBoardBody, PutUpdateBodarBody } from '../validator/Board'
 import { Board as BoardModel} from '../models/Board';
-import authorization  from '../middlewares/authorization' //权限
-
+import { getAndValidateBoard } from '../validator/Board'
 @Controller('/board')
 @Flow([authorization]) 
 export class BoardController {
@@ -14,15 +13,30 @@ export class BoardController {
         @Ctx() ctx: Context,
         @Body() body: PostAddBoardBody //校验规则
     ){
+        let {name} = body
+        console.log('body',JSON.stringify(body))
 
+        //验证--数据库中是否已经存在要注册的用户
+        let board = new BoardModel();
+        board.name = name
+        board.userId = ctx.userInfo.id
+        await board.save();
+
+        ctx.status = 201;
+        return board;
     }
 
-    // 2、获取登陆用户所有看板
+    //2、获取登陆用户所有看板
+    // http://localhost:8080/api/v1/board
     @Get('')
     async getBoards(
         @Ctx() ctx: Context
     ) {
-        
+        let where = {
+            userId: ctx.userInfo.id
+        }
+        let boards = await BoardModel.findAll({where})
+        return boards
     }
 
     //3、获取当前登陆用户指定的一个看板详情
@@ -31,7 +45,16 @@ export class BoardController {
         @Ctx() ctx: Context,
         @Params('id') id: number
     ) {
-        
+        // let board = await BoardModel.findByPk(id)
+        // if(!board){
+        //     throw Boom.notFound('指定看板不存在')
+        // }
+        // if (board.userId !== ctx.userInfo.id) {
+        //     throw Boom.forbidden('禁止访问该面板');
+        // }
+        // 如上逻辑进行封装--- getAndValidateBoard 函数
+        let board = await getAndValidateBoard(id, ctx.userInfo.id)
+        return board;
     }
 
     //4、更新指定看板
@@ -41,7 +64,12 @@ export class BoardController {
         @Params('id') id: number,
         @Body() body: PutUpdateBodarBody
     ){
+        let board = await getAndValidateBoard(id, ctx.userInfo.id);
+        // 更新
+        board.name = body.name || board.name;
+        await board.save();
 
+        ctx.status = 204;
     }
 
     //5、删除指定面板
@@ -50,7 +78,9 @@ export class BoardController {
         @Ctx() ctx: Context,
         @Params('id') id: number
     ){
-        
-    }
+        let board = await getAndValidateBoard(id, ctx.userInfo.id);
+        await board.destroy();
 
+        ctx.status = 204;
+    }
 }
--- 全栈项目案例学习/trello/backend/src/validator/Board.ts
+++ 全栈项目案例学习/trello/backend/src/validator/Board.ts
@@ -4,6 +4,8 @@ import {
     MaxLength,
     ValidateIf
 } from 'class-validator';
+import {Board as BoardModel} from "../models/Board";
+import Boom from "@hapi/boom";
 
 class BoardBody {
     @Length(1, 50, {
@@ -33,4 +35,18 @@ export class PutUpdateBodarBody {
         message: '面板名称不能超过255个字符'
     })
     name: string;
+}
+
+export async function getAndValidateBoard(id: number, userId: number): Promise<BoardModel> {
+    let board = await BoardModel.findByPk(id);
+
+    if (!board) {
+        throw Boom.notFound('指定看板不存在');
+    }
+
+    if (board.userId !== userId) {
+        throw Boom.forbidden('禁止访问该面板');
+    }
+
+    return board;
 }
